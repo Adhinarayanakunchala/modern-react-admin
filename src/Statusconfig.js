8a99b7df39da85c3e62dd0f975ad90e2a594728b
Adinarayana kunchala <137266024+Adhinarayanakunchala@users.noreply.github.com>
2025-07-13 14:06:58 +0530

    Delete src directory
    wefegfrf

--- src/Statusconfig.js
+++
@@ -1,1 +0,0 @@
-export const AdminCancelType = 1
--- src/s3config.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-  bucketName: "bandham",
-  dirName: "Banners",
-  region: "ap-south-1",
-  accessKeyId: process.env.REACT_APP_S3_ACCESSKEY,
-  secretAccessKey: process.env.REACT_APP_S3_SECRET,
-};
